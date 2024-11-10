7d80d5f7f608b78f95322be9ee6e14299f0f4343
mustakim-rafid <mustakimrafid126@gmail.com>
2024-11-11 00:46:00 +0600

    add cloudinary file upload with multer

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,22 @@
+import { v2 as cloudinary } from 'cloudinary';
+import fs from "fs"
+
+cloudinary.config({ 
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,  
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        if(!localFilePath) return null
+        const uploadFile = await cloudinary.uploader.upload(localFilePath, { resource_type: "auto" })
+        console.log("File uploaded to cloudinary successfully", uploadFile.url);
+        return uploadFile
+    } catch (error) {
+        fs.unlinkSync(localFilePath) // remove the locally saved file as the upload operation got failed
+        return null
+    }
+}
+
+export {uploadOnCloudinary}
